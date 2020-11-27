e5502acd58a344e973884e12bea88212e45d5bc8
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-11-27 17:27:08 +0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      display: ["Poppins", "sans-serif"],
+      body: ["Poppins", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        "primary" : "#009688",
+        "primary-light" : "#52C7B8",
+        "primary-dark" : "#00675B",
+        "accent" : "#23B987"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
