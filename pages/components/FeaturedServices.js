ee03e56228b0cd24fb0d81a93c37bf2d165ebf21
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-11-27 21:06:20 +0800

    Added feautured services section

---
+++ pages/components/FeaturedServices.js
@@ -0,0 +1,9 @@
+const FeaturedServices = () => {
+    return(
+        <section className="w-10/12 mx-auto p-6">
+            <h1>Popular Services</h1>
+        </section>
+    )
+}
+
+export default FeaturedServices
