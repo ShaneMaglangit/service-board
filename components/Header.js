e6e1d6af4767c3bb1a43bd4f61702162a88bfd80
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-11-29 04:48:08 +0800

    Fixed Typo

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
             <Navbar position="absolute top-0" showSearch={false} isLight={false} />
             <div className="flex-shrink h-full mx-auto flex flex-col items-center justify-center">
                 {/* Header text */}
-                <h1 className="text-3xl text-white font-extrabold font-display text-center">Enjoy the best services a the <br />comforts of your own home.</h1>
+                <h1 className="text-3xl text-white font-extrabold font-display text-center">Enjoy the best services at the <br />comforts of your own home.</h1>
                 {/* Search bar */}
                 <Search classes="w-10/12 mt-4" />
             </div>
