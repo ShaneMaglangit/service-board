99487eeac4b0f6a53a22fb5b4aa3f4ad4096a4c4
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-11-29 02:19:17 +0800

    Added categories functionality

--- components/CategoryBar.js
+++ components/CategoryBar.js
@@ -1,15 +1,15 @@
 import Link from 'next/link'
 
 const CategoryBar = () => {
-    var categories = ["Carpentry", "Plumbing", "Hair Dresser", "Make-up Artist", "Tutor", "Car Wash", "Laundry", "Computer Repair", "Cleaning Services"]
+    var categories = ["Carpentry", "Plumbing", "Grooming", "Make-up Artist", "Tutor", "Car Wash", "Laundry", "Repair", "Cleaning Services"]
 
     return(
         <div className="w-full shadow py-4">
             <ul className="w-11/12 mx-auto flex justify-around font-display text-sm font-semibold">
                 {categories.map((category) => 
                     <li key={category}>
-                        <Link href="/">
-                            <a className="border-b-2 border-transparent hover:border-primary transition-all duration-100 ease-in">{category}</a>
+                        <Link href={`/listing?category=${category}`}>
+                            <a  onClick={() => window.location.href=`/listing?category=${category}`}className="border-b-2 border-transparent hover:border-primary transition-all duration-100 ease-in">{category}</a>
                         </Link>
                     </li>
                 )}
