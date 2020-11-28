b11ef4b51e4f5e97b66fae1f043917c6d5db5ac9
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-11-28 20:17:28 +0800

    Featured Changes

--- pages/components/FeaturedServices.js
+++ pages/components/FeaturedServices.js
@@ -1,23 +1,39 @@
 import Service from "../classes/Service"
 import HorizontalServiceCard from "./HorizontalServiceCard"
 import Link from 'next/link'
-
-var tempService = [
-    new Service("1", null, "Haircut", "Grooming", "Service includes cutting, styling, and treatment of the hair", 5, 12.00, true, "Sampaloc, Manila", "David's Salon", "1"),
-    new Service("2", null, "Haircut", "Grooming", "Service includes cutting, styling, and treatment of the hair", 5, 12.00, true, "Sampaloc, Manila", "David's Salon", "2"),
-    new Service("3", null, "Haircut", "Grooming", "Service includes cutting, styling, and treatment of the hair", 5, 12.00, true, "Sampaloc, Manila", "David's Salon", "3"),
-    new Service("4", null, "Haircut", "Grooming", "Service includes cutting, styling, and treatment of the hair", 5, 12.00, true, "Sampaloc, Manila", "David's Salon", "4"),
-    new Service("5", null, "Haircut", "Grooming", "Service includes cutting, styling, and treatment of the hair", 5, 12.00, true, "Sampaloc, Manila", "David's Salon", "5"),
-    new Service("6", null, "Haircut", "Grooming", "Service includes cutting, styling, and treatment of the hair", 5, 12.00, true, "Sampaloc, Manila", "David's Salon", "6")
-]
+import { useState, useEffect } from 'react'
+import firebase from "firebase"
+require("firebase/firestore")
 
 const FeaturedServices = () => {
+    const[services, setServices] = useState([])
+
+    useEffect(() => {
+        if(services.length === 0) {
+            const db = firebase.default.firestore()
+            db.collection("services")
+                .where("featured", "==", true)
+                .orderBy("rating", "desc")
+                .limit(6)
+                .get()
+                .then(snapshot => {
+                    setServices(snapshot.docs.map((doc) => {
+                        const data = doc.data()
+                        return new Service(doc.id, data.thumbnail, data.title, data.category, data.description, data.rating, data.estimatedCost, data.isHourly, data.location, data.provider, data.providerId)
+                    }))
+                })
+                .catch((error) => {
+                    console.log("Error getting documents: ", error);
+                })
+        }
+    }, [services])
+
     return(
         <section className="w-10/12 mx-auto py-12">
             <h1 className="text-xl font-bold font-display">Popular Services</h1>
             <div className="flex flex-wrap mt-4">
-                {tempService.map((service, i) => 
-                    <div key="{service.id}" className={`${i % 2 === 0 ? "pr-4" : ""} w-1/2 pb-4`}>
+                {services.map((service, i) => 
+                    <div key={service.id} className={`${i % 2 === 0 ? "pr-4" : ""} w-1/2 pb-4`}>
                         <HorizontalServiceCard service={service} /> 
                     </div>
                 )}
--- pages/components/HorizontalServiceCard.js
+++ pages/components/HorizontalServiceCard.js
@@ -1,8 +1,16 @@
+import Link from 'next/link'
+
 const ServiceCard = ({service}) => {
+    const thumbnailStyle = {
+        backgroundImage: `url(${service.thumbnail})`,
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+    }
+
     return (
         <div className="flex items-stretch shadow-md">
-            <div className="h-40 w-48 bg-primary"></div>
-            <div className="flex flex-col justify-center flex-grow p-6">
+            <div className="h-40 w-48 bg-gray-200" style={thumbnailStyle}></div>
+            <div className="flex flex-col justify-center flex-1 p-6">
                 <h3 className="font-display font-semibold">{service.title}</h3>
                 <p className="font-body text-sm">{service.description}</p>
             </div>
